b0a96abc3f6189f0725f95e1b288e4733cf9e0a9
flansma <login0937@gmail.com>
2023-03-02 20:03:47 +0900

    Backup: 2023-03-02 20:03:47

--- 가볍게 시작하는 통계학/ISLR/ISLR-Practices/3-8-b.R
+++ 가볍게 시작하는 통계학/ISLR/ISLR-Practices/3-8-b.R
@@ -1,8 +1,5 @@
-#
-plot(mpg_power)
+#Auto 데이터셋으로부터 최소제곱회귀선을 구해 scatter plot을 그리고 그래프 이름을 설정
+plot(mpg~horsepower,main= "Scatter plot of mpg & horsepower", data=Auto)
 
-# scatter plot of mpg and horsepower
-plot(Auto$horsepower, Auto$mpg, xlab="Horsepower", ylab="MPG")
-
-# add least squares regression line to plot
-abline(lm(mpg ~ horsepower, data = Auto))
+#회귀선을 추가
+abline(lm(mpg~horsepower, data=Auto), lwd =3, col ="red")
